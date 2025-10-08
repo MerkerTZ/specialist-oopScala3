6765a1c806e57d2dd3cc17b1c9f9145bffac9465
timi9364@gmail.com <timi9364@gmail.com>
2025-10-08 12:11:30 +0300

    day1 вспомогательный конструктор this

--- src/main/scala/ScalaDay1.scala
+++ src/main/scala/ScalaDay1.scala
@@ -1,7 +1,7 @@
 class ScalaDay1
 
 class Point(var x:Int, var y:Int) {
-
+  def this() = this(0,0)
   def this(c:Int) = this(c,c)
 
   val color: String = "red"
@@ -17,8 +17,11 @@ object ScalaDay1 {
     val p1 = Point(0, 0)
     p1.setXY(3, 7)
     val p2 = Point(2)
+    val p3 = Point()
 
     println(s"Point1: ${p1.x}, ${p1.y}")
     println(s"Point2: ${p2.x}, ${p2.y}")
+    println(s"Point3: ${p3.x}, ${p3.y}")
+
   }
 }
